ac1954aead70d2fc6a6ec9c68ebcb07dc8db126f
cjkobs <ckobs@asu.edu>
2022-10-12 21:03:02 -0700

    i have no idea how to fix this error,

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import math
 # also, A12 will be represented as a[0] while A21 will be a[1]
 pres = lambda a: ((0.5*math.exp(a[0]*((a[1]*0.5)/(a[0]*0.5+a[1]*0.5))**2))*17.473+(0.5*math.exp(a[1]*((a[0]*0.5)/(a[1]*0.5+a[0]*0.5))**2))*28.824-36.7)**2
 # initial guesses for A12 and A21
-A_init = [5, 5]
+A_init = [100, 1]
 # constraints for the problem, it's gonna be every other combination in this list
 constr = ({"type": "eq", "fun": lambda a: ((0.1*math.exp(a[0]*((a[1]*0.9)/(a[0]*0.1+a[1]*0.9))**2))*17.473+(0.9*math.exp(a[1]*((a[0]*0.1)/(a[1]*0.1+a[0]*0.9))**2))*28.824-34.4)**2},
           {"type": "eq", "fun": lambda a: ((0.2*math.exp(a[0]*((a[1]*0.8)/(a[0]*0.2+a[1]*0.8))**2))*17.473+(0.8*math.exp(a[1]*((a[0]*0.2)/(a[1]*0.2+a[0]*0.8))**2))*28.824-36.7)**2},
@@ -23,5 +23,5 @@ bnds = ((0, 1000), (0, 1000))
 
 res = minimize(pres, A_init, method='SLSQP', bounds=bnds, constraints=constr)
 
-print(res.a)
+print(res.x)
 #why
